84eeac6867cc0704a8ee25ad4da471b61ec6ef60
Debrup Chatterjee <debrupchatterjee@Debrups-MacBook-Air.local>
2023-04-16 20:54:20 +0530

    using WSGI interface

--- app.py
+++ app.py
@@ -20,5 +20,5 @@ def get_links():
     else:
         return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
