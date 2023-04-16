a976b8f110cfa2586a1f66a687c58b54f0426d70
Debrup Chatterjee <debrupchatterjee@Debrups-MacBook-Air.local>
2023-04-16 14:58:46 +0530

    Adding all files

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+from bs4 import BeautifulSoup
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/links', methods=['GET', 'POST'])
+def get_links():
+    if request.method == 'POST':
+        url = request.form['url']
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        links = soup.find_all('a')
+        urls = [link.get('href') for link in links]
+        return render_template('links.html', urls=urls)
+    else:
+        return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
